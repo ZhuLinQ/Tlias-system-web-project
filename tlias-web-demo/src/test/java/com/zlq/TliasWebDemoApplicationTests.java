6fcec7475dd20757bc87c8de7c59c61e740431c6
guchasen <3347736124@qq.com>
2026-03-09 12:18:08 +0800

    Initial commit

---
+++ tlias-web-demo/src/test/java/com/zlq/TliasWebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TliasWebDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
